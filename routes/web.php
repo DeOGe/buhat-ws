11ce150f74d793a596c16fa871937b2aaf5fb586
Gerhard <deocampogerhard@gmail.com>
2025-07-08 22:36:23 +0800

    remove google auth in web route

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\GoogleController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Welcome to the API',
+        'status' => 'success'
+    ]);
 });
 
-Route::get('/auth/google', [GoogleController::class, 'redirectToGoogle']);
-Route::get('/auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+// Route::get('/auth/google', [GoogleController::class, 'redirectToGoogle']);
+// Route::get('/auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+
+// testing
